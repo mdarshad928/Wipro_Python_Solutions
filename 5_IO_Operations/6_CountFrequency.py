d63cd050a7199d0b2c0ab8b98071913d97fa3cb3
Arshad <66003496+mdarshad928@users.noreply.github.com>
2022-02-23 06:19:55 +0000

    added 6th solution of IO Capsule.

--- 5_IO_Operations/6_CountFrequency.py
+++ 5_IO_Operations/6_CountFrequency.py
@@ -1,16 +1,33 @@
-#Incomplete
-inp = input("Enter the word: ")
-count = 0
-f1 = open('file.txt')
-x = f1.readlines()
-t = []
-for i in x:
-  t = i.split()
-  print("********************")
-  for j in t:
-    print(j)
-    print(f'j is {j}, input is {inp}')
-    if j == x:
-      count+=1
-f1.close()
-print("Total Count is: ", count)
+inp = input("Enter a word: ")
+# Open the file in read mode
+text = open("file.txt", "r")
+
+# Create an empty dictionary
+d = dict()
+
+# Loop through each line of the file
+for line in text:
+	# Remove the leading spaces and newline character
+	line = line.strip()
+
+	# Convert the characters in line to
+	# lowercase to avoid case mismatch
+	line = line.lower()
+
+	# Split the line into words
+	words = line.split(" ")
+
+	# Iterate over each word in line
+	for word in words:
+		# Check if the word is already in dictionary
+		if word in d:
+			# Increment count of word by 1
+			d[word] = d[word] + 1
+		else:
+			# Add the word to dictionary with count 1
+			d[word] = 1
+
+# Print the contents of dictionary
+print(d[inp])
+
+# Credit - GeeksforGeeks
