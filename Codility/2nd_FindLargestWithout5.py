67fdff32f1d2687663d3a88ba4875e8e5024b35e
Arshad <66003496+mdarshad928@users.noreply.github.com>
2022-05-18 22:28:36 +0000

    Added java version in codility 2nd question.

--- Codility/2nd_FindLargestWithout5.py
+++ Codility/2nd_FindLargestWithout5.py
@@ -16,3 +16,28 @@ def solution(N):
 
 if __name__ == "__main__":
     print(solution(555555))
+
+
+
+###### JAVA SOLUTION #####
+# public class Main
+# {
+# 	public static int solution(int N){
+# 	    String str = Integer.toString(N);
+# 	    int count = str.length() - str.replace("5","").length();
+# 	    int index = -1;
+# 	    int max = Integer.MIN_VALUE;
+# 	    for (int i = 0; i<count; i++){
+# 	        index = str.indexOf("5", index+1);
+# 	        String s = str.substring(0,index)+str.substring(index+1, str.length());
+# 	        if (Integer.parseInt(s)>max){
+# 	            max = Integer.parseInt(s);
+# 	        }
+# 	    }
+# 	    return max;
+# 	}
+	
+# 	public static void main(String[] args) {
+# 		System.out.println(solution(5000));
+# 	}
+# }
